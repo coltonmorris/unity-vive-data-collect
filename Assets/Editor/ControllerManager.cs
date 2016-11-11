8d4896c9c02f8a7069395b8b67a5f49fb2517f6c
coltonmorris <coltonmorris94@gmail.com>
2016-11-11 14:10:13 -0700

    helllo git

---
+++ Assets/Editor/ControllerManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+public class ControllerManager : MonoBehaviour
+{
+    public bool triggerButtonDown = false;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private SteamVR_Controller.Device controller
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
+    }
+
+    private SteamVR_TrackedObject trackedObj;
+    void Start()
+    {
+        trackedObj = GetComponent();
+    }
+
+    void Update()
+    {
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+        triggerButtonDown = controller.GetPressDown(triggerButton);
+        if (triggerButtonDown)
+        {
+            Debug.Log("Save some data!");
+        }
+    }
+}
---
+++ Assets/WandController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO;
+using System.Globalization;
+
+public class WandController : MonoBehaviour
+{
+    public bool triggerButtonDown = false;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private ushort pulsePower = 700;
+    private SteamVR_Controller.Device controller
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
+    }
+
+    private SteamVR_TrackedObject trackedObj;
+    void Start()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        //WriteLine("hand_x,hand_y,hand_z,hand_rot_x,hand_rot_y,hand_rot_z,elbow_x,elbow_y,elbow_z,elbow_rot_x,elbow_rot_y,elbow_rot_z");
+    }
+
+    void Update()
+    {
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+        triggerButtonDown = controller.GetPressDown(triggerButton);
+
+        if (triggerButtonDown)
+        {
+            // haptic feedback to know we touched the button
+            controller.TriggerHapticPulse(pulsePower);
+            // Default parameter is the touchpad. Vector2 is returned and goes from (-1,-1) to  (1,1)
+            Vector2 touchpad = controller.GetAxis();
+            Debug.Log("Transform, this is probably what we want: " + controller.transform);
+            Debug.Log("Touchpad x: " + touchpad.x);
+            Debug.Log("Touchpad y: " + touchpad.y);
+            Debug.Log("Save some data!");
+            Debug.Log("Controller angular velocity: " + controller.angularVelocity);
+            Debug.Log("Touchpad: " + touchpad);
+            Debug.Log("Controller velocity: " + controller.velocity);
+            
+        }
+    }
+    void WriteLine(string line)
+    {
+        string time = DateTime.Now.ToString("dd_MM_yyyy_HH_mm");
+        string path = @"C:\Temp\" + time + ".csv";
+        using (var w = new StreamWriter(path, true))
+        {
+            // test data format:
+            // hand_x, hand_y, hand_z, hand_rot_x, hand_rot_y, hand_rot_z, elbow_x, elbow_y, elbow_z, elbow_rot_x, elbow_rot_y, elbow_rot_z
+            w.WriteLine(line);
+        }
+    }
+}
